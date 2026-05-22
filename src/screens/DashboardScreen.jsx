5fcf8d77648a32ca1506aec02c3efdf327494d94
heartk466 <heartdkho@gmail.com>
2026-05-23 00:38:21 +0800

    fix

--- src/screens/DashboardScreen.jsx
+++ src/screens/DashboardScreen.jsx
@@ -10,7 +10,7 @@ import {
 import './DashboardScreen.css';
 import TopBar from '../components/TopBar';
 import BottomNav from '../components/BottomNav';
-import Chatdrawer from '../components/ChatDrawer';       
+import ChatDrawer from '../components/ChatDwer';       
 import { fetchAllHouseholdExpenses, UTILITY_CATEGORIES } from '../utils/expenseUtils';
 import { useAppContext } from '../AppContext';
 
